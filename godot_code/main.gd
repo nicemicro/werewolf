f8a0bae095162facfdc8a18523b4f733a39ccf73
nicemicro <63175285+nicemicro@users.noreply.github.com>
2024-02-24 23:32:06 +0900

    very basic main menu (mouse driven) added to the godot side

--- godot_code/main.gd
+++ godot_code/main.gd
@@ -1,15 +1,34 @@
 extends Control
 
+@onready var menuPlace: Control = $MainWindow/Columns/MenuCol
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	menuPlace.get_child(0).connect("startButtonPressed", openGameJoin)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func openGameJoin():
+	var lobbyScene = preload("res://player_lobby.tscn")
+	for child in menuPlace.get_children():
+		child.queue_free()
+	var lobbyNode: VBoxContainer = lobbyScene.instantiate()
+	lobbyNode.connect("backToMain", openMainMenu)
+	menuPlace.add_child(lobbyNode)
+
+func openMainMenu():
+	var menuScene = preload("res://main_menu.tscn")
+	for child in menuPlace.get_children():
+		child.queue_free()
+	var menuNode: VBoxContainer = menuScene.instantiate()
+	menuNode.connect("startButtonPressed", openGameJoin)
+	menuPlace.add_child(menuNode)
 
 func _on_game_nite_controlpads_message_received(client, message):
 	print("received <%s> from <%s>" % [message, client])
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
